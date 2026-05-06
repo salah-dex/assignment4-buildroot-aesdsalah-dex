b12d443fad50f11d1618ed7c0c45ddac0bfcf3f9
Salaheddine GHANEM <ghanemsalaheddine1@gmail.com>
2026-05-06 17:17:41 +0200

    try on Https git ref

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -10,7 +10,7 @@ AESD_ASSIGNMENTS_VERSION = 340425d1bfaaefc6ff67847dfb8aaa0008a57eb2
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = git@github.com:salah-dex/assignment3-aesdsalah-dex-.git
+AESD_ASSIGNMENTS_SITE = https://github.com/salah-dex/assignment3-aesdsalah-dex-.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
